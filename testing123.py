3e6efbcc13c5ead1dff3607920c0b899dbbf9729
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2019-01-14 22:17:19 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	EggRoulette.py

--- testing123.py
+++ testing123.py
@@ -1,16 +1,26 @@
 import pygame
+
 pygame.init()
 
-screen = pygame.display.set_mode((640, 480)) # Set screen size of pygame window
-background = pygame.Surface(screen.get_size())  # Create empty pygame surface
-background.fill((255, 255, 255))     # Fill the background white color (red,green,blue)
-background = background.convert()  # Convert Surface to make blitting faster
+pygame.display.set_mode((500, 500))
 
-screen.blit(background, (0, 0))
+pygame.display.set_caption('First Game')
 
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        mainloop = False # pygame window closed by user
-    elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_ESCAPE:
-            mainloop = False # user pressed ES
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+while run:
+
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+
+            run = False
+
+pygame.quit()
