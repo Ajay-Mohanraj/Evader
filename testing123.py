73eb3de267cedb6b186fb2b66a0b82cf090065c4
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2019-01-20 18:19:47 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	EggRoulette.py

--- testing123.py
+++ testing123.py
@@ -53,38 +53,6 @@ class Player(object):
 
             if keys[pygame.K_DOWN] and self.y < (500 - self.h - self.vel):
                 self.y += self.vel
-            """if not(isJump):
-        
-                if keys[pygame.K_UP] and y > vel:
-        
-                    y -= vel
-        
-                if keys[pygame.K_DOWN]and y < (500 - height - vel):
-        
-                    y += vel
-        
-                if keys[pygame.K_SPACE]:
-        
-                    isJump = True
-        
-            else:
-        
-                if jumpCount >= -10:
-        
-                    neg = 1
-        
-                    if jumpCount < 0:
-        
-                        neg = -1
-        
-                    y -= (jumpCount ** 2) ** 0.5 * neg
-                    jumpCount -= 1
-        
-            else:
-        
-                isJump = False
-                jumpCount = 10
-            """
 
 
 ball_list = []
